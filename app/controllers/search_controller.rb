bfa2981003ac12c727eccf4b8a2bbae119e7466d
Allison Brakel <abrakel135@gmail.com>
2018-03-22 14:01:27 +0000

    Full search functionality and fixed css

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
     @search_terms = params[:q]
-    @products = Product.where('artist_name LIKE "%' + @search_terms + '%"')
+    @products = Product.where('artist_name LIKE "%' + @search_terms + '%" or album_name LIKE "%' + @search_terms + '%"');
   end
 end
