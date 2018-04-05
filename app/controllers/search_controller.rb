31e32747b66ab15a38e342aa57ecb7ed010c6c4b
Allison Brakel <abrakel135@gmail.com>
2018-04-05 16:25:26 +0000

    Search by category

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,13 @@
 class SearchController < ApplicationController
   def index
     @search_terms = params[:q]
-    @products = Product.where('artist_name LIKE "%' + @search_terms + '%" or name LIKE "%' + @search_terms + '%"');
+
+    if params[:category_id].blank?
+      @products = Product.where('artist_name LIKE "%' + @search_terms + '%" or name LIKE "%' + @search_terms + '%"');
+    else
+      category = Category.find(params[:category_id])
+      @products = category.products.where('name LIKE ? or artist_name LIKE ?',
+                                          "%#{@search_terms}%", "%#{@search_terms}%")
+    end
   end
 end
