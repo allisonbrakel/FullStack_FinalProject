10c7864aad9b6a1d616963619b6d696155595561
Allison Brakel <abrakel135@gmail.com>
2018-04-05 01:38:32 +0000

    Adding active admin for many to many

---
+++ app/admin/orders.rb
@@ -0,0 +1,31 @@
+ActiveAdmin.register Order do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+#
+  permit_params :total, :subtotal, :gst, :pst, :hst, :user_id, :status,
+                product_orders_attributes: [:id, :order_id, :product_id, :price, :quantity, :_destroy]
+
+  form do |f|
+    f.semantic_errors *f.object.errors.keys
+
+    f.inputs "Order" do
+      f.input :total
+      f.has_many :product_orders, allow_destroy: true do |n_f|
+        n_f.input :product
+      end
+    end
+
+    f.actions
+  end
+
+end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,7 @@ class Order < ApplicationRecord
 
   has_many :product_orders
   has_many :products, through: :product_orders
+
+  accepts_nested_attributes_for :product_orders, allow_destroy: true
+
 end
